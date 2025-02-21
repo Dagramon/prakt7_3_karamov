dc72a38cf58442ad3523cc479ab66a4652da63b3
Dagramon <karamov-damir228@mail.ru>
2025-02-21 18:33:28 +0500

    1

--- src/24.kt
+++ src/24.kt
@@ -8,7 +8,7 @@ fun main()
         val d2 = readln().toDouble()
         when
         {
-            d1 <= 0 && d2 <= 0 -> println("Диагонали не могут быть меньше либо равны нулю")
+            d1 <= 0 || d2 <= 0 -> println("Диагонали не могут быть меньше либо равны нулю")
             else ->
             {
                 val s = Math.round((d1 * d2) / 2)
